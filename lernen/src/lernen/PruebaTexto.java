4a4ac28bea3c77402680f358420bc6c2f7b44345
BelenEsc <belenescobari@gmail.com>
2023-04-21 22:37:45 +0100

    casa test correo

--- lernen/src/lernen/PruebaTexto.java
+++ lernen/src/lernen/PruebaTexto.java
@@ -2,6 +2,7 @@ package lernen;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -37,7 +38,7 @@ class Layer extends JPanel {
 	private JTextField campo;
 
 	public Layer() {
-		
+
 		campo = new JTextField(6);
 		add(campo);
 		JButton boton1 = new JButton("aceptar");
@@ -45,17 +46,44 @@ class Layer extends JPanel {
 		boton1.addActionListener(mievento);
 		add(boton1);
 
-		c
-
 	}
 
 	private class DameTexto implements ActionListener {
 		public DameTexto() {
+
 		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println(campo.getText().trim());
+			String result = campo.getText().trim();
+
+			Integer positionPoint = null;
+			Integer positionArroba = null;
+			boolean point = false;
+			boolean arroba = false;
+			int countpoint = 0;
+			int countarroba = 0;
+
+			for (int i = 0; i < result.length(); i++) {
+				if (result.charAt(i) == '.') {
+					countpoint++;
+					positionPoint = i;
+				}
+				if (result.charAt(i) == '@') {
+					countarroba++;
+					positionArroba = result.indexOf("@");
+				}
+			}
+			if (countpoint != 0 && countarroba == 1) {
+				if (positionArroba != null && positionPoint != null && positionArroba < positionPoint
+						&& positionArroba != 0) {
+					System.out.println("correo valido");
+				} else {
+					System.out.println("correo invalido1");
+				}
+			} else {
+				System.out.println("correo invalido2");
+			}
 		}
 	}
 }
