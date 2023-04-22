c58a0d64f6d16b7ad3ed875df2a10c7042577d62
Belen Escobari <b.escobari@bo.berlin>
2023-04-22 18:01:42 +0200

    bgbn

---
+++ lernen/src/lernen/ModificacionTexto.java
@@ -0,0 +1,72 @@
+package lernen;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+
+public class ModificacionTexto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Marco marco2 = new Marco();
+	}
+
+}
+
+class Marco extends JFrame {
+
+	public Marco() {
+
+		setBounds(50, 50, 500, 500);
+		setTitle("modificaciones");
+		setDefaultCloseOperation(3);
+		setVisible(true);
+		Layer2 layer = new Layer2();
+		add(layer);
+
+	}
+
+}
+
+class Layer2 extends JPanel {
+
+	public Layer2() {
+
+		JTextField texto = new JTextField("aqui un texto", 20);
+		add(texto);
+		
+		Receptora receptora = new Receptora(); // este objeto tiene todas las instrucciones  para cada caso. pe. q pasa si se borra,anade, cambia, etc pero todavia hay que hacer algo con estas instrucciones
+		
+		Document midoc = texto.getDocument(); // el metodoo getDocument es de la clase jtextfield que al aplicarlo sobre
+												// un elemento de esa clase, devuelve un objeto de tipo document. este objeto contiene el texto del campo texto y sobre este tienen que actuar las acciones cuando se ins/del.
+		midoc.addDocumentListener(receptora); //esta linea dice que se van a ahcer con las instrucciones. Osea al cambiar el texto (accion): actuar sobre el objeto midoc y pasarle las instrucciones de que hacer en cada caso
+
+	}
+
+	private class Receptora implements DocumentListener {
+
+		public Receptora() {
+
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			System.out.println("introducido");
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			System.out.println("removed");
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+}
--- lernen/src/lernen/PruebaTexto.java
+++ lernen/src/lernen/PruebaTexto.java
@@ -1,11 +1,12 @@
 package lernen;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -14,6 +15,7 @@ public class PruebaTexto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MarcoText marcoTexto = new MarcoText();
+		marcoTexto.setDefaultCloseOperation(3);
 	}
 
 }
@@ -22,11 +24,9 @@ class MarcoText extends JFrame {
 
 	public MarcoText() {
 
-		setBounds(100, 100, 300, 350);
-		setDefaultCloseOperation(3);
+		setBounds(600,300,600,350);
 		setVisible(true);
 		setTitle("texto");
-
 		Layer lamina = new Layer();
 		add(lamina);
 	}
@@ -36,23 +36,35 @@ class MarcoText extends JFrame {
 class Layer extends JPanel {
 
 	private JTextField campo;
+	private JLabel output;
 
 	public Layer() {
+		
+		setLayout(new BorderLayout()); //con este metodo estamos dividiendo la capa Layer en 5 partes, a,i,d,a,centro
+		
+		JPanel arriba = new JPanel ();  //crear una nueva capa
+		arriba.setLayout(new FlowLayout()); // en esta capa. setearle un layout de tipo flowlayout q es en una misma fila. quiere decir que todos los elementos que se anadan a esta capa van a tener una alineacion en una fila la sitaxis es objetoSobreElQueSeQuiereHacerAlgo.loQueSeLeQuiereHacer(UsandoQue)
+		
+		JLabel etiqueta = new JLabel("introducir el correo"); //se crea otra capa con texto;
+		arriba.add(etiqueta); //a la capa "arriba" addear la etiqueta. como arriba se ha descrito, este elemento anadido a la capa se va a alinear en una fila con todos los demas q se le agrege a esta capa
+		campo = new JTextField(20); //se crea un recuadro de texto de tamanio 6
+		arriba.add(campo); //a la capa "arriba tb se le agrega este elemento
 
-		campo = new JTextField(6);
-		add(campo);
-		JButton boton1 = new JButton("aceptar");
-		DameTexto mievento = new DameTexto();
-		boton1.addActionListener(mievento);
-		add(boton1);
+		JButton boton1 = new JButton("aceptar"); //se crea un boton q se tiene como texto aceptar
+		arriba.add(boton1); //se le agrega este elemento a la capa "arriba" tb 
+		
+		DameTexto mievento = new DameTexto(); //crear un objeto de tipo ActionListener por que el metodo addactionlistener NECESITA un objeto de ese tipo con todas las instrucciones 
+		boton1.addActionListener(mievento); //sobre el boton1. la accion a ejecutar es click y se le pasa las instrucciones que se almacenaron en el objeto mievento "el oyente" que lleva el metodo actionperformed 
+		output = new JLabel("",JLabel.CENTER); //JLabel es una capa para agregar texto. Se inicia sin texto y ubicado en el centro 
+		add(output,BorderLayout.CENTER); //finalmente se dice que posicion va a tener la capa output( que es un solo texto)
+		add(arriba,BorderLayout.NORTH); // se dice que posicion va a tener la capa arriba que contiene 3 elementos 
 
 	}
 
 	private class DameTexto implements ActionListener {
 		public DameTexto() {
-
 		}
-
+		
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String result = campo.getText().trim();
@@ -77,12 +89,12 @@ class Layer extends JPanel {
 			if (countpoint != 0 && countarroba == 1) {
 				if (positionArroba != null && positionPoint != null && positionArroba < positionPoint
 						&& positionArroba != 0) {
-					System.out.println("correo valido");
+					output.setText("valido"); //este metodo dice sobre el elemento output (q es una capa de texto libre settearle el texto Valido
 				} else {
-					System.out.println("correo invalido1");
+					output.setText("invalido");
 				}
 			} else {
-				System.out.println("correo invalido2");
+				output.setText("invalido");
 			}
 		}
 	}
