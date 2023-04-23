dfc76b18121eb0f9657b07f77d319342e99d96c2
BelenEsc <belenescobari@gmail.com>
2023-04-23 22:47:54 +0200

    casa domingo2

--- lernen/src/lernen/BotonRadio.java
+++ lernen/src/lernen/BotonRadio.java
@@ -36,83 +36,120 @@ class Marco6 extends JFrame {
 }
 
 class Lamina6 extends JPanel {
-	JRadioButton botonRadio1;
-	JRadioButton botonRadio2;
-	JRadioButton botonRadio3;
-	JRadioButton botonRadio4;
-	JRadioButton botonRadio5;
+
+	JPanel panelRadio;
 	JLabel textField;
+	ButtonGroup grupoBotones;
 
 	public Lamina6() {
 
 		setLayout(new BorderLayout());
-
-		JPanel panelRadio = new JPanel();
-		JPanel panelTexto = new JPanel();
-
+		panelRadio = new JPanel();
 		textField = new JLabel("aqui hay un texto");
-		panelTexto.add(textField);
-
-//		Document docTexto = new ;
-
+		grupoBotones = new ButtonGroup();
 		panelRadio.setLayout(new FlowLayout());
 
-		botonRadio1 = new JRadioButton("chico", false);
-		botonRadio2 = new JRadioButton("chico-mediano", false);
-		botonRadio3 = new JRadioButton("mediano", false);
-		botonRadio4 = new JRadioButton("medio grande", false);
-		botonRadio5 = new JRadioButton("grande", false);
-
-		ButtonGroup grupoBotones = new ButtonGroup();
-
-		grupoBotones.add(botonRadio1);
-		grupoBotones.add(botonRadio2);
-		grupoBotones.add(botonRadio3);
-		grupoBotones.add(botonRadio4);
-		grupoBotones.add(botonRadio5);
-
-		Instrucciones instrucciones = new Instrucciones();
-		botonRadio1.addActionListener(instrucciones);
-		botonRadio2.addActionListener(instrucciones);
-		botonRadio3.addActionListener(instrucciones);
-		botonRadio4.addActionListener(instrucciones);
-		botonRadio5.addActionListener(instrucciones);
-
-		// panelRadio.add(grupoBotones);//no se agrega el grupo a la lamina !!! el grupo
-		// solo sirve para usar la funcion de no poder marcar varios objetos al mismo
-		// tiempo, pero no es una lamina en si q se pueda agregar a la lamina panel
-
-		panelRadio.add(botonRadio1);
-		panelRadio.add(botonRadio2);
-		panelRadio.add(botonRadio3);
-		panelRadio.add(botonRadio4);
-		panelRadio.add(botonRadio5);
-
-		add(panelTexto, BorderLayout.NORTH);
+		crearBotones("chico", false, 12);
+		crearBotones("mediano", false, 16);
+		crearBotones("grande", false, 20);
+		crearBotones("super grande", false, 24);
+		
+		add(textField, BorderLayout.NORTH);
 		add(panelRadio, BorderLayout.SOUTH);
 	}
 
-	class Instrucciones implements ActionListener {
+	public void crearBotones(String nombre, boolean inicio, int tamanio) {
+		
+		JRadioButton boton = new JRadioButton(nombre, inicio);
+		grupoBotones.add(boton);
+		panelRadio.add(boton);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			int sizeFont = 0;
-			if (e.getSource() == botonRadio1) {
-				sizeFont = 8;
-			} else if (e.getSource() == botonRadio2) {
-				sizeFont = 12;
-			} else if (e.getSource() == botonRadio3) {
-				sizeFont = 16;
-			} else if (e.getSource() == botonRadio4) {
-				sizeFont = 20;
-			} else if (e.getSource() == botonRadio5) {
-				sizeFont = 24;
+		ActionListener instrucciones = new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				textField.setFont(new Font("", Font.PLAIN, tamanio));
 			}
+		};
 
-			textField.setFont(new Font("", Font.PLAIN, sizeFont));
-
-		}
+		boton.addActionListener(instrucciones);
 
 	}
 
+//	JRadioButton botonRadio1;
+//	JRadioButton botonRadio2;
+//	JRadioButton botonRadio3;
+//	JRadioButton botonRadio4;
+//	JRadioButton botonRadio5;
+//	JLabel textField;
+//
+//	public Lamina6() {
+//
+//		setLayout(new BorderLayout());
+//
+//		JPanel panelRadio = new JPanel();
+//		JPanel panelTexto = new JPanel();
+//
+//		textField = new JLabel("aqui hay un texto");
+//		panelTexto.add(textField);
+//
+////		Document docTexto = new ;
+//
+//		panelRadio.setLayout(new FlowLayout());
+//
+//		botonRadio1 = new JRadioButton("chico", false);
+//		botonRadio2 = new JRadioButton("chico-mediano", false);
+//		botonRadio3 = new JRadioButton("mediano", false);
+//		botonRadio4 = new JRadioButton("medio grande", false);
+//		botonRadio5 = new JRadioButton("grande", false);
+//
+//		ButtonGroup grupoBotones = new ButtonGroup();
+//
+//		grupoBotones.add(botonRadio1);
+//		grupoBotones.add(botonRadio2);
+//		grupoBotones.add(botonRadio3);
+//		grupoBotones.add(botonRadio4);
+//		grupoBotones.add(botonRadio5);
+//
+//		Instrucciones instrucciones = new Instrucciones();
+//		botonRadio1.addActionListener(instrucciones);
+//		botonRadio2.addActionListener(instrucciones);
+//		botonRadio3.addActionListener(instrucciones);
+//		botonRadio4.addActionListener(instrucciones);
+//		botonRadio5.addActionListener(instrucciones);
+//
+//		// panelRadio.add(grupoBotones);//no se agrega el grupo a la lamina !!! el grupo
+//		// solo sirve para usar la funcion de no poder marcar varios objetos al mismo
+//		// tiempo, pero no es una lamina en si q se pueda agregar a la lamina panel
+//
+//		panelRadio.add(botonRadio1);
+//		panelRadio.add(botonRadio2);
+//		panelRadio.add(botonRadio3);
+//		panelRadio.add(botonRadio4);
+//		panelRadio.add(botonRadio5);
+//
+//		add(panelTexto, BorderLayout.NORTH);
+//		add(panelRadio, BorderLayout.SOUTH);
+//	}
+//
+//	class Instrucciones implements ActionListener {
+//
+//		@Override
+//		public void actionPerformed(ActionEvent e) {
+//			int sizeFont = 0;
+//			if (e.getSource() == botonRadio1) {
+//				sizeFont = 8;
+//			} else if (e.getSource() == botonRadio2) {
+//				sizeFont = 12;
+//			} else if (e.getSource() == botonRadio3) {
+//				sizeFont = 16;
+//			} else if (e.getSource() == botonRadio4) {
+//				sizeFont = 20;
+//			}else if (e.getSource()==botonRadio5) {
+//				sizeFont = 24;}
+//
+//			textField.setFont(new Font("", Font.PLAIN, sizeFont));
+//
+//		}
+//
+//	}
 }
