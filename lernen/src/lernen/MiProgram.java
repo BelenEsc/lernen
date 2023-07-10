bc4b4b17f559b32e1678bae5fbc22b0e817c17f4
BelenEsc <belenescobari@gmail.com>
2023-07-10 23:52:03 +0200

    elimina filas y columnas sin modificar contenido y undo columnas solo

--- lernen/src/lernen/MiProgram.java
+++ lernen/src/lernen/MiProgram.java
@@ -1,6 +1,7 @@
 package lernen;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -8,9 +9,14 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -19,9 +25,10 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 public class MiProgram {
 	public static void main(String[] args) {
@@ -63,24 +70,63 @@ class Frame extends JFrame {
 		panelTexto = new PanelTexto();
 		add(panelTexto);
 		guardarMenuItem.addActionListener(new GuardarCSVActionListener(panelTexto.table));
+		JMenuItem eliminarFilas = new JMenuItem("Eliminar Filas");
+		JMenuItem eliminarColumnas = new JMenuItem("Eliminar Columnas");
+		eliminarColumnas.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panelTexto.eliminarColumnasSeleccionadas();
+			}
+		});
+		menu.add(eliminarColumnas);
+		eliminarFilas.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panelTexto.eliminarFilasSeleccionadas();
+			}
+		});
+		menu.add(eliminarFilas);
 	}
 
 	class PanelTexto extends JPanel {
-		JTextArea textArea;
-		JScrollPane scrollPane;
+		CustomTableModel model;
 		JTable table;
 		JScrollPane tableScrollPane;
+		Stack<TableState> undoStack;
 
 		public PanelTexto() {
 			setLayout(new BorderLayout());
 			table = new JTable();
 			tableScrollPane = new JScrollPane(table);
+			model = new CustomTableModel();
+			table.setModel(model);
+			undoStack = new Stack<>();
+
 			add(tableScrollPane, BorderLayout.CENTER);
+
+			JButton undoButton = new JButton("Undo");
+			undoButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					undo();
+				}
+			});
+			JButton undoRowsButton = new JButton("Undo Rows");
+			undoRowsButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					undoRowsDeletion();
+				}
+			});
+			JPanel x = new JPanel();
+			x.setLayout(new FlowLayout());
+			x.add(undoRowsButton);
+			x.add(undoButton);
+			add(x, BorderLayout.SOUTH);
 		}
 
 		public void cargarTabla(String[][] datos, String[] columnas) {
 			DefaultTableModel model = new DefaultTableModel(datos, columnas) {
-
 				@Override
 				public boolean isCellEditable(int row, int column) {
 					return false; // Hacer que las celdas no sean editables
@@ -88,8 +134,110 @@ class Frame extends JFrame {
 			};
 
 			table.setModel(model);
-
 			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		}
+
+		public void eliminarFilasSeleccionadas() {
+			int[] selectedRows = table.getSelectedRows();
+
+			if (selectedRows.length > 0) {
+				DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+				// Eliminar filas en orden descendente para evitar cambios en los índices
+				for (int i = selectedRows.length - 1; i >= 0; i--) {
+					model.removeRow(selectedRows[i]);
+				}
+			}
+		}
+
+		public void eliminarColumnasSeleccionadas() {
+			int[] selectedColumns = table.getSelectedColumns();
+
+			if (selectedColumns.length > 0) {
+				TableColumnModel columnModel = table.getColumnModel();
+				TableState currentState = new TableState(columnModel);
+
+				for (int i = selectedColumns.length - 1; i >= 0; i--) {
+					int selectedColumn = selectedColumns[i];
+					TableColumn tableColumn = columnModel.getColumn(selectedColumn);
+					columnModel.removeColumn(tableColumn);
+					currentState.addDeletedColumn(tableColumn);
+				}
+
+				undoStack.push(currentState);
+			}
+		}
+
+		public void undo() {
+			if (!undoStack.isEmpty()) {
+				TableState previousState = undoStack.pop();
+				TableColumnModel columnModel = table.getColumnModel();
+
+				for (TableColumn tableColumn : previousState.getDeletedColumns()) {
+					columnModel.addColumn(tableColumn);
+					columnModel.moveColumn(columnModel.getColumnCount() - 1, tableColumn.getModelIndex());
+				}
+
+				for (Object[] rowData : previousState.getDeletedRows()) {
+					model.addRow(rowData);
+				}
+			}
+		}
+
+		public void undoRowsDeletion() {
+			if (!undoStack.isEmpty()) {
+				TableState previousState = undoStack.pop();
+
+				for (Object[] rowData : previousState.getDeletedRows()) {
+					model.addRow(rowData);
+				}
+			}
+		}
+
+		class CustomTableModel extends DefaultTableModel {
+			public Object[] getRowData(int row) {
+				Object[] rowData = new Object[getColumnCount()];
+
+				for (int i = 0; i < getColumnCount(); i++) {
+					rowData[i] = getValueAt(row, i);
+				}
+
+				return rowData;
+			}
+		}
+
+		class TableState {
+			private TableColumnModel columnModel;
+			private List<Object[]> deletedRows;
+			private Stack<TableColumn> deletedColumns;
+
+			public TableState() {
+				this.columnModel = null;
+				this.deletedRows = new ArrayList<>();
+				this.deletedColumns = new Stack<>();
+			}
+
+			public TableState(TableColumnModel columnModel) {
+				this.columnModel = columnModel;
+				this.deletedRows = new ArrayList<>();
+				this.deletedColumns = new Stack<>();
+			}
+
+			public void addDeletedColumn(TableColumn tableColumn) {
+				deletedColumns.push(tableColumn);
+			}
+
+			public void setDeletedRows(List<Object[]> deletedRows) {
+				this.deletedRows = deletedRows;
+			}
+
+			public List<Object[]> getDeletedRows() {
+				return deletedRows;
+			}
+
+			public Stack<TableColumn> getDeletedColumns() {
+				return deletedColumns;
+			}
 		}
 	}
 
@@ -151,7 +299,7 @@ class Frame extends JFrame {
 			if (saveReturnVal == JFileChooser.APPROVE_OPTION) {
 				String saveFilePath = saveFileChooser.getSelectedFile().getAbsolutePath();
 				guardarTablaComoCSV(saveFilePath);
-//				establecerSoloLectura(saveFilePath);
+//              establecerSoloLectura(saveFilePath);
 			}
 		}
 
@@ -186,24 +334,22 @@ class Frame extends JFrame {
 				System.out.println("Error al guardar la tabla como archivo CSV: " + ex.getMessage());
 			}
 		}
+	}
 
-//		private void establecerSoloLectura(String filePath) {
-//			try {
-//				Path path = Path.of(filePath);
-//
-//				// Establecer atributos de solo lectura
-//				Set<PosixFilePermission> permissions = new HashSet<>();
-//				permissions.add(PosixFilePermission.OWNER_READ);
-//				permissions.add(PosixFilePermission.OWNER_WRITE);
-//				permissions.add(PosixFilePermission.GROUP_READ);
-//				permissions.add(PosixFilePermission.GROUP_WRITE);
-//				permissions.add(PosixFilePermission.OTHERS_READ);
-//				permissions.add(PosixFilePermission.OTHERS_WRITE);
-//				Files.setPosixFilePermissions(path, permissions);
-//				System.out.println("Archivo establecido como solo lectura: " + filePath);
-//			} catch (IOException ex) {
-//				System.out.println("Error al establecer el archivo como solo lectura: " + ex.getMessage());
-//			}
-//		}
+	class CustomTableModel extends DefaultTableModel {
+		public void removeColumn(int columnIndex) {
+			// Remove the column header
+			columnIdentifiers.remove(columnIndex);
+
+			// Remove the column data from each row
+			for (Vector<Object> row : dataVector) {
+				if (columnIndex < row.size()) {
+					row.remove(columnIndex);
+				}
+			}
+
+			// Update the table view
+			fireTableStructureChanged();
+		}
 	}
 }
